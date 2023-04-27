5ca40283bcc9fc3352a4e69b2a8504e7a6f83ce3
Lasha Zhizhiashvili <Lashalovesbasketball927@gmail.com>
2023-04-27 10:36:06 +0400

    Add project files.

---
+++ C#/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("enter two number:");
+
+int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("choose what to do with number?");
+Console.WriteLine("------------------------------");
+Console.WriteLine("press 1 for Addition");
+Console.WriteLine("press 2 for Subtraction");
+Console.WriteLine("press 3 for Multiplication");
+Console.WriteLine("press 4 for Division");
+
+string sub = Console.ReadLine();
+int og = 4689853;
+
+switch (sub)
+{
+    case "1":
+        Console.WriteLine(num1 + num2);
+        break;
+
+    case "2":
+        Console.WriteLine(num1 - num2);
+        break;
+
+    case "3":
+        Console.WriteLine(num1 * num2);
+        break;
+
+    case "4":
+        Console.WriteLine(num1 / num2);
+        break;
+
+    default:
+        Console.WriteLine("you didnt enter a valid number");
+        break;
+}
