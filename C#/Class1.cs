412836cef316cad51dbe061145557e07ff0b1fe0
Lasha Zhizhiashvili <Lashalovesbasketball927@gmail.com>
2023-06-10 11:17:01 +0400

    dsada

---
+++ C#/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_
+{
+    internal class Algorithm
+    {
+        public static bool All<T>(List<T> list, Func<T, bool> comparer)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (!comparer(list[i]))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static bool Any<T>(List<T> list, Func<T, bool> comparer)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (comparer(list[i]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static T Distinct<T>(T[] mass, Func<T, bool> comparer)
+        {
+            for (int i = 0; i < mass.Length; i++)
+            {
+                if (comparer(mass[i]))
+                {
+                    return mass[i];
+                }
+            }
+            return default;
+        }
+    }
+}
